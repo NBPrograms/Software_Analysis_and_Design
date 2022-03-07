36045cd95781331156db6794100c19281f318b4d
NBoes <nicholas.2000@att.net>
2022-03-07 16:14:10 -0600

    Midterm Problems
    Problems from my midterm completed in an hour and 45 minutes. Good design was key in the grading. One used association (aggregation) and the other used abstraction.

---
+++ Midterm Problems/CarEmployee.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MT_Honest_Auto_Salary {
+    internal class CarEmployee {
+        static void Main(string[] args) {
+            //First: Get the Employee Data
+            List<Employee> employees = getTheEmployeeData();
+
+            //Second: Output the employees
+            outputEmployees(employees);
+
+            Console.ReadLine();
+        }
+
+        private static void outputEmployees(List<Employee> employees) {
+            Console.WriteLine("--------------SALES--------------");
+            foreach (Employee emp in employees) {
+                Console.WriteLine(emp.ToString());
+            }
+        }
+
+        private static List<Employee> getTheEmployeeData() {
+            //Initialization
+            List<Employee> employees = new List<Employee>();
+
+            //Adding employees
+            employees.Add(new FullEmployee("Sally", "Full Time", 10, 40000));
+            employees.Add(new FullEmployee("Sal", "Full Time", 9, 40000));
+            employees.Add(new FullEmployee("Silvia", "Full Time", 9, 100000));
+            employees.Add(new PartEmployee("Sammie", "Part Time", 0, 10000));
+
+            return employees;
+        }
+    }
+
+    /// Employee abstract class
+    /// Takes in their name, type, experience, and total sales
+    /// Has abstract method that determines their sales commission
+    /// Enscapsulates variables with capital letters
+    /// Adjusts for input in the experience and total sales attributes
+    /// Overrides toString
+    public abstract class Employee {
+        private string name;
+        private string type;
+        private int yearXP;
+        private double totalSales;
+
+        protected Employee(string name, string type, int yearXP, double totalSales) {
+            Name = name;
+            Type = type;
+            YearXP = yearXP;
+            TotalSales = totalSales;
+        }
+
+        public abstract double getSalesCommission();
+
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Type {
+            get { return type; }
+            set { type = value; }
+        }
+
+        public int YearXP {
+            get { return yearXP; }
+            set {
+                if (value < 0) {
+                    yearXP = 0;
+                } else if (value >= 0) {
+                    yearXP = value;
+                } else {
+                    Console.WriteLine("Error-Bad Input:{0}", value);
+                }
+            }
+        }
+
+        public double TotalSales {
+            get { return totalSales; }
+            set {
+                if (value < 0) {
+                    totalSales = 0;
+                } else if (value >= 0) {
+                    totalSales = value;
+                } else {
+                    Console.WriteLine("Error-Bad Input:{0}", value);
+                }
+            }
+        }
+
+        public override string ToString() {
+            return string.Format("Name:{0}\tSales Type:{1}\tSales Amount:${2}\tCommission:${3}",
+                name, type, totalSales, getSalesCommission());
+        }
+    }
+
+    /// FullEmployee Class
+    /// Extends the Employee Class
+    /// If yearXP >= 10, get a 6% commission
+    /// Otherwise gets a 5% commission
+    /// Adjusts for invalid values
+    public class FullEmployee : Employee {
+        public FullEmployee(string name, string type, int yearXP, double totalSales) 
+            : base(name, type, yearXP, totalSales) {
+        }
+
+        public override double getSalesCommission() {
+            const double COM_RATE = .05;
+            const double XP_RATE = .06;
+
+            if (YearXP >= 10) {
+                return TotalSales * XP_RATE;
+            } else if (YearXP >= 0) {
+                return TotalSales * COM_RATE;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    /// PartEmployee Class
+    /// Extends the Employee Class
+    /// Gets a 4% commission
+    /// Adjusts for invalid values
+    public class PartEmployee : Employee {
+        public PartEmployee(string name, string type, int yearXP, double totalSales) 
+            : base(name, type, yearXP, totalSales) {
+        }
+
+        public override double getSalesCommission() {
+            const double COM_RATE = .04;
+
+            if (YearXP >= 0) {
+                return TotalSales * COM_RATE;
+            } else {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Midterm Problems/CarLot.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MT_Honest_Auto_Lot {
+    internal class CarLot {
+        static void Main(string[] args) {
+            //First: Read in the cars
+            List<Car> cars = getTheCars();
+
+            //Second: Output the cars
+            outputCars(cars);
+
+            Console.ReadLine();
+        }
+
+        private static void outputCars(List<Car> cars) {
+            foreach (Car car in cars) {
+                Console.WriteLine(car.ToString() + "\n");
+            }
+        }
+
+        private static List<Car> getTheCars() {
+            //Initialization
+            Dictionary<string, List<Option>> options = getCarOptions();
+            List<Car> cars = new List<Car>();
+
+            //Adding cars
+            cars.Add(new Car("GMC", "Sierra", 2022, 44000, options["Sierra"]));
+            cars.Add(new Car("Chevy", "Blazer", 2022, 25000, options["Blazer"]));
+            cars.Add(new Car("Buick", "Envision", 2022, 35600, options["Envision"]));
+
+            return cars;
+        }
+
+        private static Dictionary<string, List<Option>> getCarOptions() {
+            //Intialization
+            Dictionary<string, List<Option>> options = new Dictionary<string, List<Option>>();
+            List<Option> sierraOpts = new List<Option>();
+            List<Option> blazerOpts = new List<Option>();
+            List<Option> envisionOpts = new List<Option>();
+
+            //Sierra
+            sierraOpts.Add(new Option("Sensor Pack", 1000.00, "Factory"));
+            sierraOpts.Add(new Option("Sport Package", 1500.00, "Factory"));
+            sierraOpts.Add(new Option("Remote Starter", 1499.99, "After Market"));
+
+            //Blazer
+            blazerOpts.Add(new Option("Rust Prevention", 999.00, "Weird Al’s Auto Shop"));
+
+            //Envision
+            envisionOpts.Add(new Option("Sensor Pack", 1000.00, "Factory"));
+            envisionOpts.Add(new Option("Remote Starter", 1499.99, "After Market"));
+            
+            //Add to dictionary
+            options.Add("Sierra", sierraOpts);
+            options.Add("Blazer", blazerOpts);
+            options.Add("Envision", envisionOpts);
+
+            return options;
+        }
+    }
+
+    /// Car class
+    /// Takes in the make, model, year, price, and its list of options
+    /// Encapsulated variables with capital letters
+    /// Adjusts for bad input in the year and price attributes
+    /// Overrides toString
+    public class Car {
+        private string make;
+        private string model;
+        private int year;
+        private double price;
+        private List<Option> options;
+
+        public Car(string make, string model, int year, double price, List<Option> options) {
+            Make = make;
+            Model = model;
+            Year = year;
+            Price = price;
+            Options = options;
+        }
+
+        public string Make {
+            get { return make; }
+            set { make = value; }
+        }
+
+        public string Model {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public int Year {
+            get { return year;}
+            set {
+                if (value < 0) {
+                    year = 0;
+                } else if (value >= 0) {
+                    year = value;
+                } else {
+                    Console.WriteLine("Error-Bad value:{0}", value);
+                }
+            }
+        }
+
+        private double Price {
+            get { return price; }
+            set {
+                if (value < 0) {
+                    price = 0;
+                } else if (value >= 0) {
+                    price = value;
+                } else {
+                    Console.WriteLine("Error-Bad value:{0}", value);
+                }
+            }
+        }
+
+        public List<Option> Options {
+            get { return options; }
+            set { options = value; }
+        }
+
+        public override string ToString() {
+            StringBuilder optionStr = new StringBuilder();
+
+            foreach (Option option in options) {
+                optionStr.Append(string.Format("\n-----{0}", option.ToString()));
+            }
+
+            return string.Format("Make:{0}\tModel:{1}\tYear:{2}\tPrice:${3:0.00}{4}",
+                make, model, year, price, optionStr);
+        }
+    }
+
+    /// Option class
+    /// Takes in option name, price, and type
+    /// Encapsulated variables with capital letters
+    /// Adjusts for bad input in the price and type attributes
+    /// Overrides toString
+    public class Option {
+        private string name;
+        private double price;
+        private string type;
+
+        public Option(string name, double price, string type) {
+            Name = name;
+            Price = price;
+            Type = type;
+        }
+
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Price {
+            get { return price; }
+            set {
+                if (value < 0) {
+                    price = 0;
+                } else if (value >= 0) {
+                    price = value;
+                } else {
+                    Console.WriteLine("Error-Bad value:{0}", value);
+                }
+            }
+        }
+
+        public string Type {
+            get { return type; }
+            set {
+                if (value.ToLower().Equals("factory") || value.ToLower().Equals("after market")) {
+                    type = value;
+                } else {
+                    type = "Unknown";
+                }
+            }
+        }
+
+        public override string ToString() {
+            return string.Format("Option:{0}\tCost:${1:0.00}\tType:{2}", name, price, type);
+        }
+    }
+}
